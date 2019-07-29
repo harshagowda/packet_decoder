e9ba4345d66a6282344a760f116a11364265bc93
harsha <hkrishnegowda@webroot.com>
2019-07-29 04:14:52 -0600

    frame decoder changes as per the pdf , TODO refactor into functions pending

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
-#include "pkt_decoder.h"
+//
+//  main.cpp
+//  packet_decoder
+//
+//  Created by Harsha on 7/28/19.
+//  Copyright © 2019 Harsha. All rights reserved.
+//
+
+#include "../pkt_decoder/pkt_decoder.h"
+#include <iostream>
 #include <stdio.h>
 
 static void pkt_printer(void *ctx, size_t len, const uint8_t *data)
@@ -17,12 +26,31 @@ static void pkt_printer(void *ctx, size_t len, const uint8_t *data)
 
 int main()
 {
-    const uint8_t pkt1[] = {0x02, 0xFF, 0x10, 0x22, 0x45, 0x03};
-    
     pkt_decoder_t* decoder = pkt_decoder_create(pkt_printer, nullptr);
+    
+
+     const uint8_t pkt1[] = {0x02, 0xFF, 0x10, 0x22, 0x45, 0x03};
     
     pkt_decoder_write_bytes(decoder, sizeof(pkt1), pkt1);
     
+    const uint8_t pkt2[] = {0x02, 0xFF, 0x03, 0x02, 0x45, 0x03};
+    
+    pkt_decoder_write_bytes(decoder, sizeof(pkt2), pkt2);
+    
+    const uint8_t pkt3[] = {0x02, 0xFF, 0x02, 0xFE, 0x03};
+    
+    pkt_decoder_write_bytes(decoder, sizeof(pkt3), pkt3);
+    
+    const uint8_t pkt4[] = {0x02, 0x10, 0x22, 0x03};
+    
+    pkt_decoder_write_bytes(decoder, sizeof(pkt4), pkt4);
+    
+    
+    const uint8_t pkt5a[] = {0x02, 0xFF, 0x10};
+    const uint8_t pkt5b[] = {0x22, 0x03};
+    
+    pkt_decoder_write_bytes(decoder, sizeof(pkt5a), pkt5a);
+    pkt_decoder_write_bytes(decoder, sizeof(pkt5b), pkt5b);
     pkt_decoder_destroy(decoder);
     
     return 0;
--- pkt_decoder.cpp
+++ pkt_decoder.cpp
@@ -1,27 +1,120 @@
 #include "pkt_decoder.h"
+#include <stdlib.h>
 
-pkt_decoder_t *decoder;
-pkt_read_fn_t printf_function;
-void *callback_context;
-size_t length_packet;
-const uint8_t *packet_data;
+enum packet_markers
+{
+    start_transaction                  = 0x02,
+    end_transaction                    = 0x03,
+    escape_sequnce_begning             = 0x10,
+    unescape_value                     = 0x20,
+    escape_value_start_transaction     = 0x22,
+    escape_value_end_transaction       = 0x23,
+    escape_the_escapecharacter         = 0x30
+};
+
+enum state
+{
+    start_state               = 1,
+    data_ready_state          = 2,
+    escape_start_state        = 3,
+    end_state                 = 4,
+    invalid_data_state        = 5
+};
+
+
+struct pkt_decoder
+{
+    uint8_t decoded_packet[MAX_DECODED_PACKET_LEN];
+    size_t decode_packet_index = 0 ;
+    pkt_read_fn_t printf_function;
+    state decoder_state;
+    void * callback_context;
+};
+
 
 pkt_decoder_t* pkt_decoder_create(pkt_read_fn_t callback, void *callback_ctx)
 {
-    printf_function = callback;
-    callback_context = callback_ctx;
+    pkt_decoder_t *decoder = (pkt_decoder_t *) calloc(1, sizeof(pkt_decoder_t) ) ;
+    decoder->printf_function = callback;
+    decoder->callback_context = callback_ctx;
+    decoder->decoder_state = start_state;
     return decoder;
 }
 
 // Destructor for a pkt_decoder
 void pkt_decoder_destroy(pkt_decoder_t *decoder)
 {
-    printf_function(callback_context,length_packet,packet_data);
+    free(decoder);
 }
 
 // Called on incoming, undecoded bytes to be translated into packets
 void pkt_decoder_write_bytes(pkt_decoder_t *decoder, size_t len, const uint8_t *data)
 {
-    length_packet = len;
-    packet_data = data;
+    size_t stx_position = 0 ;
+    size_t etx_position = len;
+    
+    for(size_t frame_index = stx_position ; frame_index < etx_position ; frame_index++)
+    {
+        uint8_t data_byte = data[frame_index];
+        
+        if(decoder->decoder_state == invalid_data_state || decoder->decoder_state == end_state)
+        {
+            decoder->decoder_state = start_state;
+            decoder->decode_packet_index = 0 ;
+        }
+        
+        if(decoder->decoder_state == data_ready_state)
+        {
+            if(data_byte == start_transaction)
+            {
+                decoder->decoder_state = data_ready_state;
+                decoder->decode_packet_index = 0;
+                continue ;
+            }
+            
+            if(data_byte == end_transaction)
+            {
+                decoder->decoder_state = end_state;
+                decoder->printf_function(decoder->callback_context,decoder->decode_packet_index,decoder->decoded_packet);
+                decoder->decode_packet_index = 0 ;
+                continue;
+            }
+            
+            if(data_byte == escape_sequnce_begning)
+            {
+                decoder->decoder_state = escape_start_state;
+                continue;
+            }
+            
+            decoder->decoded_packet[decoder->decode_packet_index++] = data_byte;
+            continue;
+        }
+        
+        
+        if( decoder->decoder_state == escape_start_state)
+        {
+            if((data_byte == escape_value_start_transaction) ||
+               (data_byte == escape_value_end_transaction) ||
+               (data_byte == escape_the_escapecharacter) )
+            {
+                data_byte = data_byte ^ unescape_value;
+                decoder->decoded_packet[decoder->decode_packet_index++] = data_byte;
+                decoder->decoder_state = data_ready_state ;
+                continue;
+            }
+            else
+            {
+                decoder->decoder_state = invalid_data_state;
+                continue;
+            }
+        }
+        
+        if(decoder->decoder_state == start_state)
+        {
+            decoder->decoder_state = (data[frame_index] == start_transaction) ? data_ready_state : decoder->decoder_state;
+            continue;
+        }
+        
+    }
+    
 }
