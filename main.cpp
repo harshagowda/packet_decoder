d8d1bbf10707e03c06abad3b23503e63d8544992
harsha <hkrishnegowda@webroot.com>
2019-07-29 12:23:45 -0600

    add test case

--- main.cpp
+++ main.cpp
@@ -6,9 +6,19 @@
 //  Copyright © 2019 Harsha. All rights reserved.
 //
 
-#include "pkt_decoder.h"
+#include "../pkt_decoder/pkt_decoder.h"
 #include <iostream>
 #include <stdio.h>
+
+//global packet for test
+typedef struct expected_result
+{
+    uint8_t expect_value_buffer[MAX_DECODED_PACKET_LEN];
+    size_t length;
+    
+}expected_result;
+
+expected_result expect_result;
 
 static void pkt_printer(void *ctx, size_t len, const uint8_t *data)
 {
@@ -24,33 +34,74 @@ static void pkt_printer(void *ctx, size_t len, const uint8_t *data)
     
 }
 
+static void validate_decoder_test(void *ctx, size_t len, const uint8_t *data)
+{
+    expected_result *expected_data = (expected_result *)ctx;
+    
+    printf("TEST = ");
+    
+    for(size_t i = 0; i < len; i++)
+    {
+        if(data[i] != expected_data->expect_value_buffer[i])
+            {
+                printf("FAILED\n");
+                return ;
+            }
+    }
+    
+    printf("PASSED\n");
+    
+}
+
+
+
+void run_test_case(pkt_decoder_t* decoder, const uint8_t *packet ,size_t packet_len, const uint8_t * expected_value,size_t expected_length)
+{
+    memset(expect_result.expect_value_buffer,0,sizeof(expect_result.expect_value_buffer));
+    memcpy(expect_result.expect_value_buffer,expected_value,expected_length);
+    expect_result.length = expected_length;
+    pkt_decoder_write_bytes(decoder, packet_len, packet);
+}
+
+
+
 int main()
 {
-    pkt_decoder_t* decoder = pkt_decoder_create(pkt_printer, nullptr);
+   
+    
+    pkt_decoder_t* decoder = pkt_decoder_create(validate_decoder_test, (void *)&expect_result);
     
 
-     const uint8_t pkt1[] = {0x02, 0xFF, 0x10, 0x22, 0x45, 0x03};
+    //PASS
+    const uint8_t packet1[] = {0x02, 0xFF, 0x10, 0x22, 0x00, 0x03};
+    const uint8_t packet1_expected[] = {0xFF,0x02,0x00} ;
+    run_test_case(decoder, packet1,sizeof(packet1), packet1_expected ,sizeof(packet1_expected));
     
-    pkt_decoder_write_bytes(decoder, sizeof(pkt1), pkt1);
+    //PASS
+    const uint8_t packet2[] = {0x02, 0x10, 0x22, 0x03};
+    const uint8_t packet2_expected[] = {0x02};
+    run_test_case(decoder, packet2,sizeof(packet2), packet2_expected ,sizeof(packet2_expected));
     
-    const uint8_t pkt2[] = {0x02, 0xFF, 0x03, 0x02, 0x45, 0x03};
+    //PASS
+    const uint8_t packet3[] = {0x02, 0xFF, 0x02, 0xFE, 0x03};
+    const uint8_t packet3_expected[] = {0xFE};
+    run_test_case(decoder, packet3,sizeof(packet3), packet3_expected ,sizeof(packet3_expected));
+
+    //PASS
+    //fragmented packet
+    const uint8_t packet5a[] = {0x02, 0xFF, 0x10};
+    const uint8_t packet5b[] = {0x22, 0x03};
+    const uint8_t packet5_expected[] = {0xFF, 0x02};
     
-    pkt_decoder_write_bytes(decoder, sizeof(pkt2), pkt2);
+    run_test_case(decoder, packet5a,sizeof(packet5a), packet5_expected ,sizeof(packet5_expected));
+    run_test_case(decoder, packet5b,sizeof(packet5b), packet5_expected ,sizeof(packet5_expected));
+
+
+    //FAIL = NEGATIVE TEST CASE
+    const uint8_t packet6[] = {0x02, 0xFF, 0x02, 0xFE, 0x03};
+    const uint8_t packet6_expected[] = {0x34};
+    run_test_case(decoder, packet6,sizeof(packet6), packet6_expected ,sizeof(packet6_expected));
     
-    const uint8_t pkt3[] = {0x02, 0xFF, 0x02, 0xFE, 0x03};
-    
-    pkt_decoder_write_bytes(decoder, sizeof(pkt3), pkt3);
-    
-    const uint8_t pkt4[] = {0x02, 0x10, 0x22, 0x03};
-    
-    pkt_decoder_write_bytes(decoder, sizeof(pkt4), pkt4);
-    
-    
-    const uint8_t pkt5a[] = {0x02, 0xFF, 0x10};
-    const uint8_t pkt5b[] = {0x22, 0x03};
-    
-    pkt_decoder_write_bytes(decoder, sizeof(pkt5a), pkt5a);
-    pkt_decoder_write_bytes(decoder, sizeof(pkt5b), pkt5b);
     pkt_decoder_destroy(decoder);
     
     return 0;
