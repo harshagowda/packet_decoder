dd42df1e7bafe5fa4974770b7a7ed6ed073caca2
harshagowda <harsha.k.gowda@gmail.com>
2019-08-13 01:26:00 -0600

    make functions respective to its responsibility
    make smaller functions than a monolithic ones

--- pkt_decoder.cpp
+++ pkt_decoder.cpp
@@ -52,6 +52,60 @@ void pkt_decoder_destroy(pkt_decoder_t *decoder)
     free(decoder);
 }
 
+void handel_start_state( pkt_decoder_t *decoder,uint8_t data_byte)
+{
+    switch(data_byte)
+    {
+            //STX
+        case start_transaction:
+            decoder->decoder_state = data_ready_state;
+            decoder->decode_packet_index = 0;
+            break;
+            
+            //ETX
+        case end_transaction:
+            decoder->decoder_state = end_state;
+            decoder->printf_function(decoder->callback_context,decoder->decode_packet_index,decoder->decoded_packet);
+            decoder->decode_packet_index = 0 ;
+            break;
+            
+        case escape_sequnce_begning:
+            decoder->decoder_state = escape_start_state;
+            break;
+            
+        default:
+            decoder->decoded_packet[decoder->decode_packet_index++] = data_byte;
+            break;
+    }
+}
+
+
+void handel_escape_bytes( pkt_decoder_t *decoder,uint8_t data_byte)
+{
+    if((data_byte == escape_value_start_transaction) ||
+       (data_byte == escape_value_end_transaction) ||
+       (data_byte == escape_the_escapecharacter) )
+    {
+        data_byte = data_byte ^ unescape_value;
+        decoder->decoded_packet[decoder->decode_packet_index++] = data_byte;
+        decoder->decoder_state = data_ready_state ;
+    }
+    else
+    {
+        decoder->decoder_state = invalid_data_state;
+    }
+}
+
+/*reset state to start state if current state is invalid packet or end sequnce is receved */
+void reset_state_if_invalid_or_end_transaction(pkt_decoder_t *decoder)
+{
+    if(decoder->decoder_state == invalid_data_state || decoder->decoder_state == end_state)
+    {
+        decoder->decoder_state = start_state;
+        decoder->decode_packet_index = 0 ;
+    }
+}
+
 // Called on incoming, undecoded bytes to be translated into packets
 void pkt_decoder_write_bytes(pkt_decoder_t *decoder, size_t len, const uint8_t *data)
 {
@@ -70,62 +124,21 @@ void pkt_decoder_write_bytes(pkt_decoder_t *decoder, size_t len, const uint8_t *
         }
         
         
-        if(decoder->decoder_state == invalid_data_state || decoder->decoder_state == end_state)
-        {
-            decoder->decoder_state = start_state;
-            decoder->decode_packet_index = 0 ;
-        }
+        reset_state_if_invalid_or_end_transaction(decoder);
         
         //in a state of ready and copying bytes to decoder->decoded_packet buffer
         if(decoder->decoder_state == data_ready_state)
         {
-            //STX
-            if(data_byte == start_transaction)
-            {
-                decoder->decoder_state = data_ready_state;
-                decoder->decode_packet_index = 0;
-                continue ;
-            }
-            
-            //ETX
-            if(data_byte == end_transaction)
-            {
-                decoder->decoder_state = end_state;
-                decoder->printf_function(decoder->callback_context,decoder->decode_packet_index,decoder->decoded_packet);
-                decoder->decode_packet_index = 0 ;
-                continue;
-            }
-            
-            if(data_byte == escape_sequnce_begning)
-            {
-                decoder->decoder_state = escape_start_state;
-                continue;
-            }
-            
-            decoder->decoded_packet[decoder->decode_packet_index++] = data_byte;
+            handel_start_state( decoder,data_byte);
             continue;
         }
-        
         
         //handel escape sequence
         if( decoder->decoder_state == escape_start_state)
         {
-            if((data_byte == escape_value_start_transaction) ||
-               (data_byte == escape_value_end_transaction) ||
-               (data_byte == escape_the_escapecharacter) )
-            {
-                data_byte = data_byte ^ unescape_value;
-                decoder->decoded_packet[decoder->decode_packet_index++] = data_byte;
-                decoder->decoder_state = data_ready_state ;
-                continue;
-            }
-            else
-            {
-                decoder->decoder_state = invalid_data_state;
-                continue;
-            }
+            handel_escape_bytes(decoder,data_byte);
+            continue;
         }
-        
         
         //Detect the STX
         if(decoder->decoder_state == start_state)
